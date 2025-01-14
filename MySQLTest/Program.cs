d905b7b1f82afa973cfe380e656695ef43d8cf68
Geckonda <aquariusstd@yandex.ru>
2025-01-14 22:43:15 +0400

    init

---
+++ MySQLTest/Program.cs
@@ -0,0 +1,75 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace MySQLTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string database = "MyMarket";
+            // // Строка подключения к базе данных
+            string connectionString = $"Server=localhost;Database={database};User=root;Password=;Port=3306;";
+
+            // Создание подключения с использованием строки подключения
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open(); // Открытие соединения
+                    UserRepository userRepository = new UserRepository(connection); // Создаем репозиторий пользователей
+                    UserService userService = new UserService(userRepository); // Создаем сервис для работы с пользователями
+                    Run(userService); // Запускаем основной цикл программы
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Ошибка: {ex.Message}"); // Обработка ошибок
+                }
+            }
+        }
+        static void Run(UserService userService)
+        {
+            bool exit = false;
+            while (!exit)
+            {
+                PrintMenu(); // Вывод меню в консоль
+                string? command = Console.ReadLine(); // Считываем команду пользователя
+                Console.Clear(); // Очищаем консоль перед выбором команды, чтобы не засорять интерфейс
+                switch (command)
+                {
+                    case "1":
+                        userService.AddUser();
+                        break;
+                    case "2":
+                        userService.GetAll();
+                        break;
+                    case "3":
+                        userService.GetOne();
+                        break;
+                    case "4":
+                        userService.Update();
+                        break;
+                    case "5":
+                        userService.Delete();
+                        break;
+                    case "0":
+                        exit = true;
+                        break;
+                    default:
+                        Console.WriteLine("Ошибка ввода");
+                        break;
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void PrintMenu()
+        {
+            Console.WriteLine("1) Добавить пользователя");
+            Console.WriteLine("2) Вывести список всех пользователей");
+            Console.WriteLine("3) Вывести конкретного пользователя");
+            Console.WriteLine("4) Обновить пользователя");
+            Console.WriteLine("5) Удалить пользователя");
+            Console.WriteLine("0) Выход");
+        }
+    }
+}
---
+++ MySQLTest/UserService.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySQLTest
+{
+    public class UserService
+    {
+        // Репозиторий
+        private readonly UserRepository _userRepository;
+
+        // Конструктор принимает объект UserRepository для взаимодействия с БД
+        public UserService(UserRepository repository)
+        {
+            _userRepository = repository;
+        }
+
+        public void AddUser()
+        {
+            Console.WriteLine("Добавление пользователя");
+            Console.Write("Имя пользователя: ");
+            var userName = Console.ReadLine()!;
+            Console.Write("Возраст пользователя: ");
+            var userAge = int.Parse(Console.ReadLine()!); // Парсим число
+
+            // Вызываем метод Add() из нашего репозитория
+            var result = _userRepository.Add(new User(userName, userAge));
+            if(result)
+                Console.WriteLine("Пользователь добавлен успешно!");
+            else
+                Console.WriteLine("Пользователь НЕ добавлен. Что-то пошло не так!");
+        }
+
+        public void GetAll()
+        {
+            Console.WriteLine("Список всех пользователей");
+            Console.WriteLine("ID\tВозраст\tИмя");
+            var users = _userRepository.GetAll();
+            foreach (var user in users)
+                Console.WriteLine($"{user.Id}\t{user.Age}\t{user.Name}");
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine("Удаление пользователя");
+            Console.Write("Введите id пользователя: ");
+            int id = int.Parse(Console.ReadLine()!);
+
+            // Вызываем метод Delete() из нашего репозитория
+            var result = _userRepository.Delete(id);
+            if (result)
+                Console.WriteLine("Пользователь успешно удален!");
+            else
+                Console.WriteLine("Пользователь НЕ удален. Что-то пошло не так!");
+        }
+
+        public void GetOne()
+        {
+            Console.WriteLine("Вывод конкретного пользователя");
+            Console.WriteLine("Введите id пользователя: ");
+            int id = int.Parse(Console.ReadLine()!);
+            var user = _userRepository.Get(id);
+            if(user == null)
+            {
+                Console.WriteLine("Пользователь с таким id не найден.");
+                return;
+            }
+            Console.WriteLine("ID\tВозраст\tИмя");
+            Console.WriteLine($"{user.Id}\t{user.Age}\t{user.Name}");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Обновление пользователя");
+            Console.Write("Введите id пользователя: ");
+            var userId = int.Parse(Console.ReadLine()!);
+            Console.Write("Имя пользователя: ");
+            var userName = Console.ReadLine()!;
+            Console.Write("Возраст пользователя: ");
+            var userAge = int.Parse(Console.ReadLine()!);
+            var result = _userRepository.Update(new User(userId, userName, userAge));
+            if (result)
+                Console.WriteLine("Пользователь успешно обновлен!");
+            else
+                Console.WriteLine("Пользователь НЕ обновлен. Что-то пошло не так!");
+        }
+    }
+}
